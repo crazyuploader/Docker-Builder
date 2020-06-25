af157cff5f6e0a7caa7fc7fd5f3c760b65406be4
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-06-25 15:23:06 +0530

    Update test.sh
    Testing script

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git show --pretty="" --name-only | grep "Dockerfile" >> to_build.txt
+git show --pretty="" --name-only 4dda5f4e2d9d584ad1a4c1f3f692633e75f41381 | grep "Dockerfile" >> to_build.txt
 FILES=$(cat to_build.txt)
 rm to_build.txt
 for FILE in ${FILES}; do
